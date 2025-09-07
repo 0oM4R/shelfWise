f78cf9fa09e7c48cb63022a1f02d00c4c161ef3d
Omar Kassem <omarksm09@gmail.com>
2025-09-07 07:45:32 +0300

    lint: ignore unused vars that starts with _

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default defineConfig(
+export default defineConfig([
   eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      // Ignore unused function args if they start with "_"
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+]);
